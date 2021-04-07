63f190093e8f2d5a44689ac5823ef49aa3f3f3f6
DavidSilvaG <dsg1089@gmail.com>
2021-04-07 13:30:10 -0500

    include loading spinner and refresh button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!',
+      loading: true,
       commits:[]
     },
     methods:{
@@ -9,12 +9,20 @@ var app = new Vue({
             const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
             let toFormat = new Date(date)
             return `${months[toFormat.getMonth()]} ${toFormat.getDate()} - ${toFormat.getHours()}:${String(toFormat.getMinutes()+100).substring(1,3)}`
-        }
+        },
+        async getCommits(){
+            this.loading = true
+            try {
+                let data = await fetch('https://api.github.com/repos/davidsilvag/davidsilvag.github.io/commits')
+                let unorderedCommits = await data.json()
+                this.commits = unorderedCommits.sort((a,b)=> new Date(a.commit.author.date)-new Date(b.commit.author.date))
+            } catch (error) {
+                console.log(error)
+            }
+            this.loading = false
+        },
     },
-    async created(){
-        let data = await fetch('https://api.github.com/repos/davidsilvag/davidsilvag.github.io/commits')
-        unorderedCommits = await data.json()
-        this.commits = unorderedCommits.sort((a,b)=> new Date(a.commit.author.date)-new Date(b.commit.author.date))
-        console.log(this.commits)
+    created(){
+        this.getCommits()
     }
   })
