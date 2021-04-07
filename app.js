e1a5500b284122b3d27d6524a0ecb96a1bf32634
DavidSilvaG <dsg1089@gmail.com>
2021-04-07 11:36:59 -0500

    add bootstrap and connection to GithubApi

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-      commits:[{
-          commit:{
-              message: 'Hello world'
-          }
-      }]
+      message: 'Hello Vue!',
+      commits:[]
     },
+    async created(){
+        let data = await fetch('https://api.github.com/repos/davidsilvag/davidsilvag.github.io/commits')
+        this.commits = await data.json()
+        console.log(this.commits)
+    }
   })
